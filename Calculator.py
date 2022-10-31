86583fd2f9480240c2eaddeed50991e5049acefe
SiddhuNoCheat <114589200+SiddhuNoCheat@users.noreply.github.com>
2022-10-31 09:57:15 +0000

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,8 @@
-import os
-
 print("Hi This is coded by Siddhesh")
 
 a = "place holder"
 b = "place holder"
-name = "Dear Friend"
-
+name = ""
 
 def Introduction():
     global name
@@ -13,7 +10,6 @@ def Introduction():
 
     if a == "yes":
         name = input("What is your name:")
-        print("Hi", name)
     elif a == "no":
         print("Ok No Problem")
     else:
@@ -22,8 +18,7 @@ def Introduction():
 
 
 Introduction()
-
-os.system('clear')
+print("Hi", name)
 
 def raja():
     def main1():
@@ -36,7 +31,7 @@ def raja():
                 a = input("Number 1 : ")
                 a = int(a)
             except ValueError:
-                print((f"{a} is not an integer."))
+                print(f"{a} is not an integer.")
                 main1div1()
         main1div1()
         
@@ -46,11 +41,9 @@ def raja():
                 b = input("Number 2 : ")
                 b = int(b)
             except ValueError:
-                print((f"{b} is not an integer."))
+                print(f"{b} is not an integer.")
                 main1div2()
         main1div2()
-    
-    os.system('clear')
 
     main1()
     
@@ -58,6 +51,8 @@ def raja():
         global a
         global b
         global operation
+        print(f"{a} ? {b} = ")
+        print(" ")
         print("1.Addition")
         print("2.Subtraction")
         print("3.Multiplication")
@@ -65,8 +60,6 @@ def raja():
         print("1,2,3,4")
         
         operation = input("what do you want to do:")
-
-        os.system('clear')
 
         def main2Addition():
             print(f"{a} + {b} = {a+b}")
@@ -78,13 +71,25 @@ def raja():
             print(f"{a} ÷ {b} = {a/b}")
 
 
-        if operation == "Addition" or 1:
+        if operation == "Addition":
+            operation = 1
             main2Addition()
-        elif operation == "Subtraction" or 2:
+        elif operation == "Subtraction":
+            operation = 2
             main2Subtraction()
-        elif operation == "Multiplication" or 3:
+        elif operation == "Multiplication":
+            operation = 3
             main2Multiplication()
-        elif operation == "Division" or 4:
+        elif operation == "Division":
+            operation = 4
+            main2Division()
+        elif operation == '1':
+            main2Addition()
+        elif operation == '2':
+            main2Subtraction()
+        elif operation == '3':
+            main2Multiplication()
+        elif operation == '4':
             main2Division()
         else:
             print("Pls write from the given")
@@ -93,13 +98,13 @@ def raja():
     def con():
         global a
         global b
-        if operation == "Addition" or 1:
+        if operation == '1':
             a = a + b
-        elif operation == "Subtraction" or 2:
+        elif operation == '2':
             a = a - b
-        elif operation == "Multiplication" or 3:
+        elif operation == '3':
             a = a*b
-        elif operation == "Division" or 4:
+        elif operation == '4':
             a = a/b
 
         def condiv1():
@@ -110,10 +115,11 @@ def raja():
             except ValueError:
                 print(f"{b} is not an integer.")
                 condiv1()
+            print(f'Number 1 : {a}')
         condiv1()
         main2()
         final()
-    
+
     def final():
         global fin
         print("yes")
@@ -132,7 +138,6 @@ def raja():
         else:
             print("pls write from the given")
             final()
-    os.system('clear')
     main2()
     final() 
 raja()
