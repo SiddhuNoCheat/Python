4858ee1138fa8cc7d4573aad942026edd60e0fe9
SiddhuNoCheat <114589200+SiddhuNoCheat@users.noreply.github.com>
2023-02-02 18:53:52 +0530

    Add files via upload

--- Python/Lcalc.py
+++ Python/Lcalc.py
@@ -1,7 +1,30 @@
-string = "qwer";  
+import math
+
+print("Hello Friends")
+print("This is a percentage calculator made by Siddhesh")
+print("You know what percentage I mean ;)")
+
+First_Name = input("First Name = ")
+Second_Name = input("Second Name = ")
+
+
+string = First_Name + "Friend" + Second_Name;
+string = string.lower()
 freq = [None] * len(string);
 s = ""
 tw = ""
+
+print(string)
+
+def mid(string):
+    enum = list(enumerate(string))
+    lastnum = enum[-1][0] + 1
+    if lastnum % 2 == 0:
+        return ""
+    else:
+        middle = math.floor(len(enum)/2)
+        x = enum[middle][1]
+        return x
 
 for i in range(0, len(string)):  
     freq[i] = 1;  
@@ -18,22 +41,34 @@ for i in range(0, len(freq)):
     if(string[i] != ' ' and string[i] != '0'):  
         s += str(freq[i])
         print(s)
-print("\n\n")
-#if len(s)%2 != 0:
-#if "#" found with print means it was for debugging
-for i in range(0, int(len(s)/2)):
-    print(s)
-    fn = int(s[1])
-    print(s)
-    print(f"")
-    ln = int(s[i-1])
-    print(s)
-    ans = int(fn + ln)
-    s = s[1:]
-    print(s)
-    s = s[:i-1]
-    print(s)
-    s += str(ans)
 
+print("\n\n")
+
+def quickmafs(string):
+    global s
+    global tw
+    if (len(s)%2 == 0):
+        print("if")
+        for i in range(1, int(len(s)/2)):
+            tw += str(int(s[i]) + int(s[-i]))
+            print(tw)
+        i = 0
+        s = tw
+        tw = ""
+    else:
+        print("else")
+        for i in range(1, int(len(s)/2-0.5)):
+            tw += str(int(s[i]) + int(s[-i]))
+            print(tw)
+        i = 0
+        tw += mid(s)
+        s = tw
+        tw = ""
+
+    if int(s) >= 99:
+        tw = ""
+        quickmafs(s)
+
+quickmafs(s)
 print(s)
 
