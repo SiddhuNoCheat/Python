f9fa49e9edac5433d733e41144ac4ad9ea3ba5e7
SiddhuNoCheat <114589200+SiddhuNoCheat@users.noreply.github.com>
2024-04-23 19:33:48 +0530

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -1,3 +1,27 @@
-a = "hola"
-a = a[:-1]
-print(a)
+import requests
+from bs4 import BeautifulSoup
+
+searchUrl = input("Movie Name: ")
+
+searchUrl = searchUrl.replace(" ","+")
+
+print(searchUrl)
+
+URL = f"https://ww1.moviesm4u.org/search?q={searchUrl}"
+r = requests.get(URL)
+
+soup = BeautifulSoup(r.content, "html.parser")
+#variableDeclaring
+i = 0
+Dict = {}
+
+# Now you can navigate and search the parse tree
+# For example, find all links on the page:
+for a in soup.find_all('h3'):
+    link = a.find('a', href = True)
+    if link:
+        i += 1
+        Dict[i] = link['href']
+        print(f"{i} {link.text}")
+
+
